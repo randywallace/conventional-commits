ebe6a3cdbde5e1675a321c01ea1ad471bc94a048
Randy D. Wallace Jr. <randy.d.wallace2.ctr@mail.mil>
2025-05-23 12:16:24 -0400

    feat(prepare-commit-msg): fiddle with prepare-commit-msg to add some selection capability

--- git-hooks/prepare-commit-msg.sh
+++ git-hooks/prepare-commit-msg.sh
@@ -141,25 +141,52 @@ function render_result() {
 
 # Useful for local testing and debugging. Usage:
 # TEST=true ./git-hooks/prepare-commit-msg.sh
-[ -z "$TEST" ] && TEST=false
-[ -z "$DEBUG" ] && DEBUG=false
+set -ueo pipefail
 
-DEFAULT_BRANCH_KEY="feat"
+exec </dev/tty
 
-if [[ "$TEST" == true ]]; then
-  ORIGINAL_MSG="${1:-"!(scope)the commit message"}"
-else
-  file=$1
-  ORIGINAL_MSG=$(cat "$file")
+ORIGINAL_FILE="$1"
+COMMIT_SOURCE="$2"
+ORIGINAL_MSG="$(cat ${ORIGINAL_FILE})"
+
+# check_conventional_commit "$ORIGINAL_MSG"
+# 
+# GIT_BRANCH=${TEST_BRANCH:-"$(git rev-parse --abbrev-ref HEAD)")}
+DEFAULT_BRANCH_KEY='feat'
+BRANCH_KEY=$(normalize_key "$GIT_BRANCH" "$ORIGINAL_MSG")
+# COMMIT_SCOPE=$(normalize_scope "$ORIGINAL_MSG")
+# JIRA_TICKET=$(normalize_ticket "$GIT_BRANCH")
+# 
+# UPDATED_MSG=$(render_message "$ORIGINAL_MSG" "$BRANCH_KEY" "$COMMIT_SCOPE" "$JIRA_TICKET")
+
+TYPES=($BRANCH_KEY fix feat build chore ci docs style refactor perf test)
+SELECTED_TYPE=
+
+PS3="Select type: "
+select typ in "${TYPES[@]}"; do
+  SELECTED_TYPE=$typ
+  break
+done
+
+read -p "Enter Scope if any: " SELECTED_SCOPE
+
+if [[ -n "${SELECTED_SCOPE}" ]]; then
+  SELECTED_SCOPE="(${SELECTED_SCOPE})"
 fi
 
-check_conventional_commit "$ORIGINAL_MSG"
+read -p "Is this a breaking change [yn]? " BREAKING_CHANGE
 
-GIT_BRANCH=${TEST_BRANCH:-"$(git rev-parse --abbrev-ref HEAD)")}
-BRANCH_KEY=$(normalize_key "$GIT_BRANCH" "$ORIGINAL_MSG")
-COMMIT_SCOPE=$(normalize_scope "$ORIGINAL_MSG")
-JIRA_TICKET=$(normalize_ticket "$GIT_BRANCH")
+case $BREAKING_CHANGE in
+  y) BREAKING_CHANGE="!";;
+  *) BREAKING_CHANGE="";;
+esac
 
-UPDATED_MSG=$(render_message "$ORIGINAL_MSG" "$BRANCH_KEY" "$COMMIT_SCOPE" "$JIRA_TICKET")
+UPDATED_MESSAGE="${SELECTED_TYPE}${SELECTED_SCOPE}${BREAKING_CHANGE}: ${ORIGINAL_MSG}"
 
-render_result "$UPDATED_MSG" "$file"
+read -p "Ready to commit: '${UPDATED_MESSAGE}' [yn]? " READY_TO_COMMIT
+
+case $READY_TO_COMMIT in
+  y) echo "$UPDATED_MSG" > "$ORIGINAL_FILE";;
+  *) exit 1 ;;
+esac
+
